e7e69e666a66c947f3505ff8642f52f5a890f5ac
devoidoflife <vivekmalneedi@gmail.com>
2016-04-24 20:23:37 +0000

    initial commit

---
+++ start_mjpg_streamer.sh
@@ -0,0 +1,4 @@
+#start mjpg-streamer
+#env LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH mjpg_streamer -o "output_http.so -w ./www -p 1180" -i "input_raspicam.so -fps 15  -x 320 -y 240 -rot 180 -br 50 -ex off -awb off"
+
+env LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH mjpg_streamer -o "output_http.so -w ./www -p 1180" -i "input_file.so -f /home/pi/tostream"
