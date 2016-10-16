59b048664277d29f658486b78728c5e8851b57b3
Vivek Malneedi <devoidoflife@users.noreply.github.com>
2016-10-16 16:14:20 -0400

    Update start_vision.sh

--- start_vision.sh
+++ start_vision.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+#start streamer
+/home/pi/vision/start_mjpg_streamer.sh
+
+#restart vision script when client breaks connection
 until python /home/pi/vision/vision.py; do
     sleep 1
 done
